5cf8852169dc9a1e0144ff5aac88e21c4e02a141
Michael T Kolb <kolb3@purdue.edu>
2020-07-13 21:08:13 -0400

    ahhh
    pa2

---
+++ PA2/pa2_code/pa2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include "sequence.h"
+#include "shell_array.h"
+#include "shell_list.h"
+
+int main()
+{
+  int num;
+  int * array = NULL;
+
+  
+}
---
+++ PA2/sequence.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include "sequence.h"
+#include "shell_array.h"
+#include "shell_list.h"
+
+long * Generate_2p3q_Seq(int length, int * seq_size)
+{
+  
+}
